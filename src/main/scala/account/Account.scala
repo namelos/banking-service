5046dfb0fff4bedf3b708af238a3ae44b9377bdd
Fred Wang <namelos@outlook.com>
2017-03-31 22:29:58 +0800

    add date of opening to accounts

--- src/main/scala/account/Account.scala
+++ src/main/scala/account/Account.scala
@@ -1,17 +1,21 @@
 package account
 
+import java.util.Date
+
 import com.github.nscala_time.time.Imports._
 
 trait Account {
   def number: String
   def name: String
   def balance: Balance
+  def dateOfOpening: Date
 }
 
 case class CheckingAccount(
   number: String,
   name: String,
-  balance: Balance
+  balance: Balance,
+  dateOfOpening: Date
 ) extends Account
 
 trait InterestBearingAccount extends Account {
@@ -21,15 +25,17 @@ trait InterestBearingAccount extends Account {
 case class SavingAccount(
   number: String,
   name: String,
-  rateOfInterest: BigDecimal,
-  balance: Balance
+  balance: Balance,
+  dateOfOpening: Date,
+  rateOfInterest: BigDecimal
 ) extends InterestBearingAccount
 
 case class MoneyMarketingAccount(
   number: String,
   name: String,
-  rateOfInterest: BigDecimal,
-  balance: Balance
+  balance: Balance,
+  dateOfOpening: Date,
+  rateOfInterest: BigDecimal
 ) extends InterestBearingAccount
 
 trait AccountService {
