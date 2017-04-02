d0adb84a3a343437fd8e86ceb47ab4acc160eaa0
Yifan Wang <namelos@outlook.com>
2017-04-02 17:26:35 +0800

    modify account service monadic comprehension

--- src/main/scala/account/AccountService.scala
+++ src/main/scala/account/AccountService.scala
@@ -3,9 +3,9 @@ package account
 import scala.util._
 
 trait AccountService {
-  def calculateInterest[A <: InterestBearingAccount](a: A): Try[Amount] =
+  def calculateInterest[A <: InterestBearingAccount](a: A, balance: Amount): Try[Amount] =
     if (a.rateOfInterest == 0) Failure(new Exception("Interest Rate not found"))
-    else Success(a.balance.amount * a.rateOfInterest)
+    else Success(balance * a.rateOfInterest)
 
   def deductTax(interest: Amount): Try[Amount] =
     if (interest.amount < 1000)
@@ -13,17 +13,20 @@ trait AccountService {
     else
       Success(interest - (interest * 0.1))
 
-  def getCurrencyBalance(a: Account): Try[Amount] = ???
+  def getCurrencyBalance(a: Account): Try[Amount] = Success(a.balance.amount)
 
-  def getAccountFrom(no: String): Try[Account] = ???
+  def getAccountFrom(no: String): Try[Account] = Success(
+    SavingsAccount(no, "some-account-name", Balance(Amount(1000L)), today, 0.15))
 
-  def calculateNetAssetValue(a: Account, balance: Amount): Try[Amount] = ???
+  def calculateNetAssetValue(a: Account, balance: Amount, interest: Amount): Try[Amount] =
+    Success(balance + interest)
 
-  def operations: Try[(Account, Amount)] = for {
-    s <- getAccountFrom("a1")
-    b <- getCurrencyBalance(s)
-    v <- calculateNetAssetValue(s, b)
-  } yield (s, v)
+  def calculateAccountNetAssetsValue(no: String): Try[(Account, Amount)] = for {
+    account <- getAccountFrom(no)
+    balance <- getCurrencyBalance(account)
+    interest <- calculateInterest(account, balance)
+    result <- calculateNetAssetValue(account, balance, interest)
+  } yield (account, result)
 }
 
 object AccountService extends AccountService
